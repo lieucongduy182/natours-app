21805fc5f6d7c69ef1c26934f97fc9dfc0d0d223
lieucongduy182 <lieucongduy182@gmail.com>
2023-05-23 23:02:45 +0700

    implement import and delete data for dev

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import fs from 'fs';
+import { getDirPath } from '../../utils/getDirPath.js';
+import Tour from '../../models/Tour.js';
+
+// Connect Database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected Database Successfully !');
+  })
+  .catch((error) => {
+    console.log('Failed to connect Database', error);
+  });
+
+const tours = JSON.parse(
+  fs.readFileSync(getDirPath(import.meta.url, './tours-simple.json')),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Imported Data Successfully');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Deleted Data Successfully');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
