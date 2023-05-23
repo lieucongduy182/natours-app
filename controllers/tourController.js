a9da96fdd1670b02cce4959b99e615d3657870b7
lieucongduy182 <lieucongduy182@gmail.com>
2023-05-23 22:34:58 +0700

    CRUD the Tours dAPI

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,42 @@
 import Tour from '../models/Tour.js';
 
 class TourController {
-  async getAllTours(req, res) {}
+  async getAllTours(req, res) {
+    try {
+      const tours = await Tour.find();
 
-  async getTour(req, res) {}
+      return res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+          tours,
+        },
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 'fail',
+        message: error,
+      });
+    }
+  }
+
+  async getTour(req, res) {
+    try {
+      const tour = await Tour.findById(req.params.id);
+
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 'fail',
+        message: error,
+      });
+    }
+  }
 
   async createTour(req, res) {
     try {
@@ -18,21 +51,50 @@ class TourController {
     } catch (error) {
       return res.status(400).json({
         status: 'fail',
-        message: 'Invalid input data !',
+        message: error,
       });
     }
   }
   async updateTour(req, res) {
-    return res.status(200).json({
-      status: 'success',
-      message: 'Updated Successfully',
-    });
+    try {
+      const id = req.params.id;
+      const data = req.body;
+
+      const tour = await Tour.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true,
+      });
+
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 'fail',
+        message: error,
+      });
+    }
   }
   async deleteTour(req, res) {
-    return res.status(204).json({
-      status: 'success',
-      message: 'Deleted Successfully',
-    });
+    try {
+      const id = req.params.id;
+
+      await Tour.findByIdAndDelete(id, {
+        strict: true,
+      });
+      return res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 'fail',
+        message: error,
+      });
+    }
   }
 }
 
--- models/Tour.js
+++ models/Tour.js
@@ -6,15 +6,47 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name !'],
       unique: true,
+      trim: true,
     },
     price: {
       type: Number,
       required: [true, 'A tour must have a price !'],
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a  group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
       type: Number,
       default: 4.5,
     },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have  a image  cover'],
+    },
+    images: [String],
+    startDates: [Date],
   },
   {
     timestamps: {
