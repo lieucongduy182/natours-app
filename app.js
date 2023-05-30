0e7b8ff20491b06e1eb06d62aed3d779a0714a60
lieucongduy182 <lieucongduy182@gmail.com>
2023-05-30 21:42:14 +0700

    config server.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParse from 'body-parser';
 import morgan from 'morgan';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,26 +9,6 @@ const app = express();
 import toursRoute from './routes/tours.js';
 import userRoute from './routes/user.js';
 import { getDirPath } from './utils/getDirPath.js';
-
-// Connect Database
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected Database Successfully !');
-  })
-  .catch((error) => {
-    console.log('Failed to connect Database', error);
-  });
 
 // Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -48,5 +27,12 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/tours', toursRoute);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can not find ${req.originalUrl} on this server`,
+  });
+});
 
 export default app;
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
+
+// Connect Database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected Database Successfully !');
+  })
+  .catch((error) => {
+    console.log('Failed to connect Database', error);
+  });
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
